c70d107b000ffae3dd738cc4c1a6b9860f4c2c49
Roger Rocha <rogerrochabnusc@gmail.com>
2025-10-17 16:15:05 -0300

    Initial Commit

---
+++ TripBroTests/TripBroTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripBroTests.swift
+//  TripBroTests
+//
+//  Created by Roger Rocha on 17/10/25.
+//
+
+import Testing
+@testable import TripBro
+
+struct TripBroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
